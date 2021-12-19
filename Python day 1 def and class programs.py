e06e5635c6688b20da7f9dff6c56ce64765588e8
disys-indhu <95426058+disys-indhu@users.noreply.github.com>
2021-12-18 23:15:02 -0800

    Add files via upload

---
+++ Python day 1 def and class programs.py
@@ -0,0 +1,632 @@
+def Information():
+ Name="Jai Venkateswara Technologies"
+ EducationalQualification= "B.TECH"
+ DateOfBirth="2/6/1988"
+ CommunicationAddress="233/3 Ground floor new thippasandra"
+ ResidenceNumber="08025216666"
+ Mobile=9900367097
+ Email="venkatesh.db@gmail.com"
+ Total="1 years"
+ ReportingManagers="Venkatesh"
+ PANCARDNO="asu675901f"
+ print(Name,EducationalQualification,DateOfBirth,CommunicationAddress,ResidenceNumber,Mobile,Email,Total,ReportingManagers,PANCARDNO)
+ return
+Information()
+
+
+class CreatingGoogleAccount:
+ FirstName="venkatesh"
+ LastName="db"
+ ChooseUsername="jvt"
+ CreataPassword="balu"
+ ConfirmYourPassword="balu"
+ Birthday="2-6-1986"
+ Gender="male"
+ Mobile=9900367097
+ Yourcurrentemailaddress="venkatesh.db@gmail.com"
+venkatesh=CreatingGoogleAccount()
+print(venkatesh.FirstName,venkatesh.Mobile)
+
+
+def BankingSBIstatement():
+ BankName="SBI Bank"
+ Date="29/05/14"
+ Time="19:48"
+ ATMNo="SJNBL48"
+ CARDNo="xxxx xxxxx xxxx 0516"
+ BRANCHNAME="Bangalore Main Branch"
+ TxnNo="2907"
+ Responsecode="072"
+ WithDrawl="500"
+ FromAc="xxxxxxxxxx05 xx01"
+ ModRS="0"
+ AvailableBalRs="1000"
+ Website="www.statebankof india.com"
+ print(BankName,Date)
+ return
+BankingSBIstatement()
+
+
+class visitingcarddetails:
+ name="naresh"
+ mailid="naresh.jvt@gmail.com"
+ contactno="9944860792"
+ companyname="jvt"
+ designation="programmer"
+ address="230/3 2nd floor hal3rd stage"
+naresh=visitingcarddetails()
+print(naresh.mailid)
+
+def phone():
+ GP="PowerVR GT7600 (six-core graphics)"
+ Model="iphone"
+ Manufactuer="Apple"
+ Os="ios 4.0"
+ Processor="1 GHZ"
+ RAmMemory="512Mb"
+ Keyboard="Software"
+ onboardstorage="16GB/32Gb"
+ Flash="LED"
+ VideoCallcamera="VGA"
+ VideoRecording="720 F"
+ NoiceCancelling="2 microphones"
+ Expandable="No"
+ print(GP)
+ return
+phone()
+
+
+class TvDetailsSpecification:
+ Model="Lgmodel"
+ LEDlighting="Backlight w/Local Dimming"
+ Sizes="55 inches"
+ Typeof240Hz="scanning Backlight (240 HZ “effect”)"
+ WirelessHDMI="yes"
+ USB="USB 2.0 (pictures, music,videos)"
+ Builtinvideoservice="youtube"
+ priceRS="55000"
+Lgmodel=TvDetailsSpecification()
+print(Lgmodel.Sizes)
+
+class ticket:
+ TicketType="Special Entry Darshan"
+ Date="Dec 1st"
+ Day="Tuesday"
+ Time="3:30 Am" 
+ PerSlotTickets="2000"
+ BookingNo="IS151110080016"
+class personal(ticket):
+ NameofthePilgrim="venkatesh"
+ OrderNo="010600013554"
+ Email="venkateshprofessional7@gmail.com"
+ AmountinfiguresRs="220.00"
+ ProofofID="Aadhaar Card 733498928758"
+ BookedDateandTime="11062015 11:36:46"
+ NoofPersons="1"
+ NameoftheSevaorDarshan="Archana"
+ ReportingTime="4:00 AM"
+ PerformanceDateandTime="12012015 AM 4:30:00"
+ PrivilegestotheSeva="Two Small Laddu"
+ BookedTime="11:36:46"
+print(personal())
+print(ticket())
+print(personal.Date)
+
+
+ 
+def address(Name,age):
+    print(Name,age)
+    return()
+address("indhu",21)
+
+
+
+database=["emp001","emp002","emp003"]
+def employee(empid,empname,empsalary,empbonous):
+ if(empid in database):
+     print(empid,empname,empsalary,empbonous)
+ return()
+objname=employee("emp003","rahul",49876,5890)
+
+
+
+
+
+def identity(name,age,gender):
+    print(name,age,gender)
+    return
+identity("king",22,"male")
+identity("queen",23,"female")
+identity("mano",32,"female")
+
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+class CorporateInformation:
+ def __init__(self,Companyname,Designation,Salary,Location):
+     self.Companyname = Companyname
+     self.Designation = Designation
+     self.Salary = Salary #self access attributes and methods of the class in python
+     self.Location = Location
+obj=CorporateInformation("gokul","coimbatore","500000","chennai")
+print(obj.Companyname, obj.Salary)
+     
+
+
+def bank():
+ AccountNo="1135497"
+ BroadbandUserID="11128550"
+ IPAddress="10.245.97.123"
+ print(AccountNo,IPAddress)
+ return()
+bank()
+
+
+
+def download():
+ TotalMBdownloaded="21.26 GB"
+ MobiledownloadSpeed="30.05 mbps"
+ MobileuploadSpeed="32.06 mbps"
+ print(TotalMBdownloaded,MobiledownloadSpeed,MobileuploadSpeed)
+ return()
+download()
+
+
+class data:
+ to="D B Venkatesh <venkatesh.db@gmail.com>"
+ date="Fri, Nov 27, 2015 at 12:05 AM"
+ subject="Request to forward the Resumes"
+ mailedby="gmail.com"
+ signedby="gmail.com"
+obj1=data()
+print(obj1.to)
+
+
+
+class holder:
+ Name1="JVTM"
+ Age="5"
+ Gender="Male"
+ EmailAddress="info@jvtechnologies.co.in"
+ PhotoIDCard="Voter Id or Ration card .."
+ IDCardNo="ASUPB590F"
+class holder1(holder): 
+ Name1="Mr. venkatesh"
+ Age1="25"
+ Gender1="Male"
+ EmailAddress1="venkatesh.db@gmail.com"
+ MobileNo1="09900367097"
+ PhotoID1="Passport"
+ PhotoIDNo1="G3839975"
+obj2=holder()
+print(holder1.Age)
+
+
+
+def person():
+ Lastname="kumar"
+ Firstname="Pavan"
+ Middlename="Bijjavaram"
+ Driverslicensenumber="ka50201500006939"
+ Driverslicensestate="karnataka"
+ Driverslicensecountry="india"
+ Civiliantype="around india"
+ Employeetype="software engineer"
+ Dateofbirth="12-07-1992"
+ Cityofbirth="rajampet"
+ print(Lastname,Firstname)
+ return()
+person()
+
+
+
+class Appearanceinformation:
+ Weight="55"
+ Hight="5.8"
+ Eyecolour="brown"
+ Gender="male"
+ Homeaddress="6/96,rajampet,kadapa."
+ Homephone=975545445
+ Mobile=8553577745
+ State="andhrapradesh"
+ County="india"
+obj3=Appearanceinformation()
+print(obj3.Weight)
+print(obj3.Gender)
+
+
+
+def SystemProperties():
+ Rating="4.5"
+ Processor="Intel® Core™ i5-2450 CPU"
+ InstalledMemoryRAM="4.00GB"
+ SystemType="64-bit Operating System"
+ PenandTouch="No Pen or Touch Input is available for this Display" 
+ ComputerName="BALU-PC"
+ ComputerDescription="WORKGROUP"
+ WindowsEdition="Windows 7 Ultimate"
+ WindowsProductID="00426-OEM-8992662-00497"
+ print(Rating,ComputerName)
+ return()
+SystemProperties()
+
+
+
+class Theatre:
+ Theatrename="PSS multiplex"
+ Screen="screen 3"
+ moviename="JVT [ C,CPP]"
+ moviecertification="U"
+ priceperticket="Rs.1050.00"
+ Numberoftickets="5"
+ seatnumbers="c1,c2,c3,c4,c5"
+ Totalamount="Rs.750.00"
+obj4=Theatre()
+print(obj4.Screen)
+
+
+
+def HealthcareBloodtest():
+ PatientName="Sathish"
+ PatientID="10567890890"
+ Patientphone="044222738"
+ Billno=56725
+ Age=34
+ DateofBirth="15/05/1991"
+ Gender="Male"
+ Fasting="Yes"
+ PatientAddress="7f,kaveri road, velur."
+ RoomNo=105
+ DateoftimeCollectedblood="08/09/15 and 08:15"
+ Enterdate="08/09/15"
+ DateandTimeRespond="08/09/15 and  09:16"
+ print(PatientName,PatientID,Patientphone,Billno)
+ return()
+HealthcareBloodtest()
+
+
+
+class computerupsspecifications:
+ model="exide-digital600"
+ ipvoltage="230v ac"
+ frequency="50hz"
+ opvoltage="230v ac"
+ batterytypesealed="maintenance free"
+ batterycapacity="12volt 7Ah"
+ batterybackup="12 to 20 mints"
+ rechargetime="5 to 6 hours"
+ weight="6.1 kg"
+obj5=computerupsspecifications()
+print(obj5.model,obj5.ipvoltage)
+
+
+
+def visitingcarddetails():
+ name="ramesh"
+ mailid="naresh.jvt@gmail.com"
+ contactno="6944860792"
+ companyname="jvt"
+ designation="programmer"
+ address="new thippasandra"
+ print(name,mailid)
+ return()
+visitingcarddetails()
+
+
+
+class Electricbillpaidvianetbanking:
+ website="www.tnebnet.org"
+ tnebusername="gilan"
+ password=1234567
+ consumernumber=23456123
+ Billingstatus="paid/unpaid"
+ modeofpayment="netbanking"
+ choosebank="sbi"
+class user(Electricbillpaidvianetbanking):
+ username="kumar007"
+ password="1234567"
+ Transactionno="18cv21828578437"
+ paymentstatus="successful"
+obj6=user()
+print(user.password)
+
+
+
+class brand:
+ Brandname="Scott International"
+ ProductCode="APPSCOTT-INTERNSWIT610835D77A441"
+ Color="Black"
+ Size="S"
+ Material="Cotton"
+ Occasion="Casual"
+ Pattern="Solid"
+ Sleeve="Full Sleeves"
+ NeckType="Hooded"
+ Fit="Slim"
+ Gender="Men"
+class SHIPPINGDETAILS(brand):
+ EstimatedArrival="9 days"
+ ReturnPolicy="Seller will accept returns within a 15 days from date of delivery of the item"
+ BuyforRS=824
+ EffectivePriceRs=453
+obj7=SHIPPINGDETAILS()
+print(obj7.Color)
+print(obj7.BuyforRS)
+
+
+
+def TvDetailsSpecification():
+ Model="LG55LHX"
+ LEDlighting="Backlight w/Local Dimming"
+ Sizes="55 inches"
+ WirelessHDMI="yes"
+ Builtinvideoservice="youtube"
+ priceRS="55000"
+ print(Model)
+ return()
+TvDetailsSpecification()
+
+
+
+class LaptopSpecification:
+ model="LGp430"
+ os=" Windows 7 home base, Premium or professional"
+ Processor="Second Generation Intel Core i3,i5,i7  processor"
+ RAM="upto 8GB"
+ Screen="14.0 inch HD-LCD -backlit"
+ Resolution="1366*768"
+ Weight="4.28 pound"
+ Storage="320GB"
+ Graphics="Intel HD Graphics"
+ Networking="802.11 b/g/n,bluetooth 3.0"
+ Batterylife="6-cell Battery"
+ Cost="50,000"
+obj8=LaptopSpecification()
+print(obj8.os)
+
+
+
+def speedPost():
+ PostofficeDetails="New Thipasundra<423203>"
+ trackingNumber="EM423395510IN"
+ CounterNo=1
+ opcode="012"
+ Fromaddress="xxxxxx"
+ ToAddress="xxxxxx"
+ wgt="20grms"
+ Amt="39.00"
+ dateandTime="08/04/2013 16:01"
+ print(opcode)
+ return()
+speedPost()
+
+
+
+class Library:
+ BookID="12345634"
+ Bookname="C Programming"	
+ BookAuthor="Dennis Ritche"
+ IssueDate="14/01/2012"
+ DueDate="13/02/2012"
+ IDNo="2008Ec027"
+ Name="Saravanan"
+ Returnon="25/02/2012"
+ Fine=12
+obj9=Library()
+print(obj9.IDNo)
+
+
+
+def ElectricityBill():
+ SelectAccount="1-121-008-54"
+ Billingdate="20/06/2012"
+ SubDivision=1
+ RRNo=12100854
+ Name="mano"
+ Address="ooty"
+ Reading=42260
+ DueDate="21/06/2012"
+ unitsconsumed=1310
+ BillAmount=6498
+ paymentDate="30/06/2012"
+ print(RRNo)
+ return()
+ElectricityBill()
+
+
+
+class TravellingBusticket:
+ Transportdetails="Tamilnadu  State Transport Corporation Limited"
+ BusDepoName="Kanchipuram/01"
+ TicketNumber="00086655"
+ Dateandtime="06/11/2014  14:58:47"
+ From="Chennai"
+ To="pondichery"
+ Qty=2
+ cost=33
+ Total=33
+ BusNo="TN 01 N 2454"
+obj11=TravellingBusticket()
+print(obj11.To)
+
+
+
+def VehicleRegistrationCertificateBook():
+ RegistrationNumber="TN 22 CY 2917"
+ OwnersName="Sachin"
+ Swdoo="Sirinivasan"
+ Dealer="Murugan Vehicles& Services Limited"
+ ClassisNo="5793603"
+ EngineNo="0G3N72505757"
+ ClassofVehicle="LMV (CAR)"
+ makersName="Maruthi SuzUKI India LTD"
+ NoofCylinder=3
+ WeightKG=740 
+ wheelBae=2360
+ SeatingCapacity=5
+ Fuel="Petrol"
+ Cubiccylinder=796
+ print(Swdoo,Fuel)
+ return()
+VehicleRegistrationCertificateBook()
+
+
+
+class Licence:
+ DLNo="TN-41230120012921"
+ Name:"suvi"
+ SorDorWof:"Ramesh"
+ Address:"chennai"
+ DOB="6/4/1998"
+ Licencefor="M/cycl &LMV"
+ Licenceprovideon="02/05/2012"
+ validity="01/05/2032"
+obj12=Licence()
+print(obj12.DLNo, obj12.DOB)
+
+
+
+def ucpolicy():
+ BranchCode=309
+ Date="07/11/2013-12:51"
+ Totalcost="6713.00"
+ mrorMrs="saravanan"
+ PolicyNo=364164915
+ Doc="02/04/2009"
+ DueDate="04/13"
+ Premiumamount="6426.00"
+ LateFee="287.20"
+ Loaninterest="Due 00/0000 to 00/0000"
+ TotalRs="6713.20"
+ NextDue="04/2014"
+ print(Doc)
+ return()
+ucpolicy()
+
+
+
+class TicketDetails:
+ TransactionID=100000380284535	
+ PNRNo=4115023549	
+ TrainNoandName="56213 / TIRUPATI PASSR"
+ DateofBooking="14-Dec-2015"
+ Class="SLEEPERCLASS"
+ Quota="GENERAL"
+ DateofJourney="04-Jan-2016"
+ From="KJM"
+ To="TPTY"
+ BoardingAt="KJM"	
+ DateOfBoarding="04-Jan-2016"
+ ReservationUpto="TPTY"
+ ScheduledDeparture="21:00 Hrs"
+obj13=TicketDetails()
+print(obj13.Quota)
+
+
+
+def account():
+ AccountName="Mr. VENKATESH DUSETTI BABU"
+ Address="NO 233, GROUND FLOOR, 5TH MAIN"
+ "Bangalore"
+ Date="24 Dec 2015"
+ AccountNumber="00000000167856041"
+ AccountDescription="SBCHQ-GEN-PUB-IND-NONRURAL-INR"
+ Branch="NEW TIPPASANDRA, BANGALORE"
+ DrawingPower="0.00"
+ InterestRatepa="4.0"
+ CIFNo=86617689795
+ IFSCode="SBIN0015645"
+ MICRCode=560002189
+ NominationRegistered="Yes"
+ print(Address)
+ return()
+account()
+
+class names:
+ Lastname="kumar"
+ Firstname="Pavan"
+ Middlename="Bijjavaram"
+ Driverslicensenumber="ka50201500006939"
+ Driverslicensestate="karnataka"
+ Driverslicensecountry="india"
+ Civiliantype="around india"
+ Employeetype="software engineer"
+ Dateofbirth="12-07-1992"
+ Cityofbirth="rajampet"
+obj14=names()
+print(obj14.Lastname)
+
+
+
+cust=["cust1","cust2","cust3","cust4"]
+def customer(custid,custname):
+ if(custid in cust):
+     print(custid,custname)
+ return()
+obj77=customer("cust4","susanth")
+
+
+
+def Library():
+ BookID="12345634"
+ Bookname="C Programming"	
+ BookAuthor="Dennis Ritche"
+ IssueDate="14/01/2012"
+ DueDate="13/02/2012"
+ IDNo="2008Ec027"
+ Name="Saravanan"
+ Returnon="25/02/2012"
+ Fine=12
+ print(BookID)
+ return()
+Library()
+
+
+
+class ElectricityBill:
+ SelectAccount="1-121-008-54"
+ Billingdate="20/06/2012"
+ SubDivision=1
+ RRNo=12100854
+ Name="indhumathi"
+ Address="ooty"
+ Reading=42260
+ DueDate="21/06/2012"
+ unitsconsumed=1310
+ BillAmount=6498
+ paymentDate="30/06/2012"
+obj98=ElectricityBill()
+print(obj98.Name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
