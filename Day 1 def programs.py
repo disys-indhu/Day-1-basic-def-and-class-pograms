9576478952cd2223289825a2cfc59e15c104b4d7
disys-indhu <95426058+disys-indhu@users.noreply.github.com>
2021-12-19 05:47:04 -0800

    Add files via upload

---
+++ Day 1 def programs.py
@@ -0,0 +1,15 @@
+def Information():
+ Name="Jai Venkateswara Technologies"
+ EducationalQualification= "B.TECH"
+ DateOfBirth="2/6/1988"
+ CommunicationAddress="233/3 Ground floor new thippasandra"
+ ResidenceNumber="08025216666"
+ Mobile=9900367097
+ Email="venkatesh.db@gmail.com"
+ Total="1 years"
+ ReportingManagers="Venkatesh"
+ PANCARDNO="asu675901f"
+ print(Name,EducationalQualification,DateOfBirth,CommunicationAddress,ResidenceNumber,Mobile,Email,Total,ReportingManagers,PANCARDNO)
+ return
+Information()    
+
